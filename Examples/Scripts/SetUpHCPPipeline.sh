6e0e8557096b01dd28d64ff242643f5a444f5dc1
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-10-28 12:19:38 +0100

    BF: Setting up FreeSurfer in SetupHCPPipeline.sh example setup file (otherwise mri_convert can't run...)

--- Examples/Scripts/SetUpHCPPipeline.sh
+++ Examples/Scripts/SetUpHCPPipeline.sh
@@ -13,8 +13,8 @@ export FSL_DIR="${FSLDIR}"
 
 # Set up FreeSurfer (if not already done so in the running environment)
 # Uncomment the following 2 lines (remove the leading #) and correct the FREESURFER_HOME setting for your setup
-#export FREESURFER_HOME=/usr/local/bin/freesurfer
-#. ${FREESURFER_HOME}/SetUpFreeSurfer.sh > /dev/null 2>&1
+# export FREESURFER_HOME=/usr/local/bin/freesurfer
+. ${FREESURFER_HOME}/SetUpFreeSurfer.sh > /dev/null 2>&1
 
 # Set up specific environment variables for the HCP Pipeline
 : ${HCPPIPEDIR:=`pwd`}
